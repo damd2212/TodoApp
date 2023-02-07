82a6a07091c36d1626a34cdeb7fdb1d730978f0e
damd2212 <53495267+damd2212@users.noreply.github.com>
2023-02-06 21:15:14 -0500

    agregada nueva funcionalidad y mejorado

--- src/app/tasklist/tasklist.page.ts
+++ src/app/tasklist/tasklist.page.ts
@@ -19,46 +19,24 @@ export class TasklistPage implements OnInit {
   }
 
   constructor(public todoService:TodoService) {
-    /* this.tasks = [
-      {title: 'Milk', status: 'open',id:'001'},
-      {title: 'Eggs', status: 'open',id:'002'},
-      {title: 'Syrup', status: 'open',id:'003'},
-      {title: 'Pancake Mix', status: 'open',id:'004'}
-    ]; */
+
   }
 
   ngOnInit() {
     this.getTareas()
   }
 
-  addItem() {
-    let theNewTask: string|null = prompt("New Task");
 
-    if (theNewTask) {
-      //this.tasks.push({ title: theNewTask, status: 'open' });
-    }else{
-      console.log("presiona cancel");
+  markAs(slidingItem: IonItemSliding, task: Task) {
+    if (task.status==='open') {
+      task.status = "done";
+    } else {
+      task.status = "open";
     }
-
-    /* if (theNewTask !== '' || theNewTask !== null ) {
-      this.tasks.push({ title: theNewTask, status: 'open' });
-    } */
-  }
-
-  markAsDone(slidingItem: IonItemSliding, task: Task) {
-    task.status = "done";
     this.todoService.updateDoc(task,this.path,task.id)
     setTimeout(() => { slidingItem.close(); }, 1);
   }
 
-  removeTask(slidingItem: IonItemSliding, task: Task) {
-    task.status = "removed";
-    let index = this.tasks.indexOf(task);
-    if (index > -1) {
-        //Removes the task from the array at a specific position
-        this.tasks.splice(index, 1);
-    }
-  }
 
   guardarTarea(){
 
@@ -68,11 +46,6 @@ export class TasklistPage implements OnInit {
     const task:Task = new Task()
 
     if (theNewTask) {
-      //this.tasks.push({ title: theNewTask, status: 'open' });
-      /* const data = {
-        title:theNewTask,
-        status:'open'
-      } */
 
       this.data.title = theNewTask
       this.data.status = 'open'
